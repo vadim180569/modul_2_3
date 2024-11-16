8cb0f0a7b142dbd6e526dd9e9c47c310ec6858a2
vadim180569 <vadim9771505001@gmail.com>
2024-11-16 19:49:41 +0300

    Add files via upload

---
+++ modul_2_3.py
@@ -0,0 +1,39 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+elem = my_list[i]
+while elem >= 0 and i < len(my_list) > 0:
+    elem = my_list[i]
+    i += 1
+    if elem < 0:
+         break
+    elif elem == 0:
+        continue
+    print(elem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
